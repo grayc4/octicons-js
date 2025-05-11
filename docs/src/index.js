c8f7d21c613c30a1af60438840a4b20f09e61d78
grayc4 <142386274+grayc4@users.noreply.github.com>
2025-05-11 10:13:20 +0000

    Update index.js

--- docs/src/index.js
+++ docs/src/index.js
@@ -1,9 +1,11 @@
-import { icons } from './icons.js';
+import * as iconsCollection from './icons.js';
 import { makeIcon } from './makeIcon.js';
 import { makeButton } from './makeButton.js';
 import { render } from './render.js';
 import { applyStyles } from './components.js';
 import { octicon, octiconBtn, octiconLabelBtn } from './octicon-utils.js';
+
+const icons = iconsCollection;
 
 export {
   icons,
