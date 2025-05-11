ffe895d08902ee9af1e575e84ddbaae9ce0311dc
grayc4 <142386274+grayc4@users.noreply.github.com>
2025-05-11 09:48:50 +0000

    Add files via upload

---
+++ docs/icons/octiconHeading.js
@@ -0,0 +1,19 @@
+import { makeIcon } from '../src/makeIcon.js';
+
+export default makeIcon({
+  attr: {
+  "width": "16",
+  "height": "16",
+  "viewBox": "0 0 16 16",
+  "fill": "currentColor",
+  "d": "M3.75 2a.75.75 0 0 1 .75.75V7h7V2.75a.75.75 0 0 1 1.5 0v10.5a.75.75 0 0 1-1.5 0V8.5h-7v4.75a.75.75 0 0 1-1.5 0V2.75A.75.75 0 0 1 3.75 2Z"
+},
+  child: [
+  {
+    "tag": "path",
+    "attr": {
+      "d": "M3.75 2a.75.75 0 0 1 .75.75V7h7V2.75a.75.75 0 0 1 1.5 0v10.5a.75.75 0 0 1-1.5 0V8.5h-7v4.75a.75.75 0 0 1-1.5 0V2.75A.75.75 0 0 1 3.75 2Z"
+    }
+  }
+]
+});
---
+++ docs/icons/octiconHorizontalRule.js
@@ -0,0 +1,19 @@
+import { makeIcon } from '../src/makeIcon.js';
+
+export default makeIcon({
+  attr: {
+  "width": "16",
+  "height": "16",
+  "viewBox": "0 0 16 16",
+  "fill": "currentColor",
+  "d": "M0 7.75A.75.75 0 0 1 .75 7h14.5a.75.75 0 0 1 0 1.5H.75A.75.75 0 0 1 0 7.75Z"
+},
+  child: [
+  {
+    "tag": "path",
+    "attr": {
+      "d": "M0 7.75A.75.75 0 0 1 .75 7h14.5a.75.75 0 0 1 0 1.5H.75A.75.75 0 0 1 0 7.75Z"
+    }
+  }
+]
+});
