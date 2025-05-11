fe9f28e774e4f98e5a72ed6add3c840bb2d1abec
grayc4 <142386274+grayc4@users.noreply.github.com>
2025-05-11 09:42:46 +0000

    Create index.js

---
+++ docs/src/index.js
@@ -0,0 +1,19 @@
+import { icons } from './icons.js';
+import { makeIcon } from './makeIcon.js';
+import { makeButton } from './makeButton.js';
+import { render } from './render.js';
+import { applyStyles } from './components.js';
+import { octicon, octiconBtn, octiconLabelBtn } from './octicon-utils.js';
+
+export {
+  icons,
+  makeIcon,
+  makeButton,
+  render,
+  applyStyles,
+  octicon,
+  octiconBtn,
+  octiconLabelBtn
+};
+
+export * from './icons.js';
