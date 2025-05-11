dbb7af09745c47d28eb1582a0aafcc3103576656
grayc4 <142386274+grayc4@users.noreply.github.com>
2025-05-11 09:52:08 +0000

    Add files via upload

---
+++ docs/icons/octiconTriangleLeft.js
@@ -0,0 +1,19 @@
+import { makeIcon } from '../src/makeIcon.js';
+
+export default makeIcon({
+  attr: {
+  "width": "16",
+  "height": "16",
+  "viewBox": "0 0 16 16",
+  "fill": "currentColor",
+  "d": "M9.573 4.427 6.177 7.823a.25.25 0 0 0 0 .354l3.396 3.396a.25.25 0 0 0 .427-.177V4.604a.25.25 0 0 0-.427-.177Z"
+},
+  child: [
+  {
+    "tag": "path",
+    "attr": {
+      "d": "M9.573 4.427 6.177 7.823a.25.25 0 0 0 0 .354l3.396 3.396a.25.25 0 0 0 .427-.177V4.604a.25.25 0 0 0-.427-.177Z"
+    }
+  }
+]
+});
