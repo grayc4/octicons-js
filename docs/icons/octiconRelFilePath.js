236585e727a31a8f65d0f73e808c9be7ec6b643b
grayc4 <142386274+grayc4@users.noreply.github.com>
2025-05-11 09:50:35 +0000

    Add files via upload

---
+++ docs/icons/octiconRelFilePath.js
@@ -0,0 +1,19 @@
+import { makeIcon } from '../src/makeIcon.js';
+
+export default makeIcon({
+  attr: {
+  "width": "16",
+  "height": "16",
+  "viewBox": "0 0 16 16",
+  "fill": "currentColor",
+  "d": "M13.94 3.045a.75.75 0 0 0-1.38-.59l-4.5 10.5a.75.75 0 1 0 1.38.59l4.5-10.5ZM5 11.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z"
+},
+  child: [
+  {
+    "tag": "path",
+    "attr": {
+      "d": "M13.94 3.045a.75.75 0 0 0-1.38-.59l-4.5 10.5a.75.75 0 1 0 1.38.59l4.5-10.5ZM5 11.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z"
+    }
+  }
+]
+});
